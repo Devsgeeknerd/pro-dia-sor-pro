70352459d5502b448cd0df6f3dedffcb5ef6d5a9
Paulo <vanderley.1109@gmail.com>
2024-01-22 15:14:01 -0300

    atualizando conteudo

--- dia_de_sorte.py
+++ dia_de_sorte.py
@@ -1,23 +1,22 @@
-# Módulos
+# Importação de Módulos
 from random import randint
 from time import sleep
 
-# Lista
+# Inicialização de Listas
 lista = list()
-
 # Jogos
 jogos = list()
 
-# Titulo
+# Impressão do Título
 print('-=' * 15)
 print('         JOGO DIA DE SORTE         ')
 print('-=' * 15)
 
-# Pergunta quantos jogos serão gerados
+# Entrada do Usuário
 quantidade = int(input('Quantos bilhetes quer gerar? '))
 total = 1
 
-# Sorteio
+# Loop de Sorteio
 while total <= quantidade:
     contagem = 0
     while True:
@@ -31,8 +30,12 @@ while total <= quantidade:
     jogos.append(lista[:])
     lista.clear()
     total += 1
+
+# Impressão dos Jogos
 print('-=' * 6, f' SORTEANDO {quantidade} JOGOS ', '-=' * 6)
 for i, l in enumerate(jogos):
     print(f'Jogo {i+1}: {l}')
     sleep(1)
+
+# Mensagem de Boa Sorte
 print('-=' * 9, '< BOA SORTE!!! >', '-=' * 9)
